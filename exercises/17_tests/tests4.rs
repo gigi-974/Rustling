28540a0bebcaa1c6c67ce3f3a540360904dab1c8
gigi-974 <127308830+gigi-974@users.noreply.github.com>
2024-04-26 10:50:14 +0200

    Update tests4.rs

--- exercises/17_tests/tests4.rs
+++ exercises/17_tests/tests4.rs
@@ -27,22 +27,28 @@ mod tests {
     #[test]
     fn correct_width_and_height() {
         // This test should check if the rectangle is the size that we pass into its constructor
+        // Création d'une instance de Rectangle avec les valeurs 10 et 20
         let rect = Rectangle::new(10, 20);
+        // Vérification que les champs width et height de l'instance ont les bonnes valeurs
         assert_eq!(rect.width, 10); // check width
-        assert_eq!(rect.height, 20); // check height
+        assert_eq!(rect.height, 20); // check heigh
     }
 
     #[test]
+    // Ajout de l'attribut #[should_panic] pour indiquer que ce test doit lever une panique
     #[should_panic]
     fn negative_width() {
         // This test should check if program panics when we try to create rectangle with negative width
+        // Création d'une instance de Rectangle avec une largeur négative (-10)
         let _rect = Rectangle::new(-10, 10);
     }
 
     #[test]
+    // Ajout de l'attribut #[should_panic] pour indiquer que ce test doit lever une panique
     #[should_panic]
     fn negative_height() {
         // This test should check if program panics when we try to create rectangle with negative height
+        // Création d'une instance de Rectangle avec une hauteur négative (-10)
         let _rect = Rectangle::new(10, -10);
     }
 }
