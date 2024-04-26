9d15f6b9b0b7fae75330aa80bc94e6953ea86982
gigi-974 <127308830+gigi-974@users.noreply.github.com>
2024-04-26 10:01:03 +0200

    Update generics2.rs

--- exercises/14_generics/generics2.rs
+++ exercises/14_generics/generics2.rs
@@ -3,11 +3,16 @@
 
 // Execute `rustlings hint generics2` or use the `hint` watch subcommand for a hint.
 
+// Ajout du type générique <T> pour permettre à Wrapper de stocker n'importe quel type
+
 struct Wrapper<T> {
     value: T,
 }
 
+// Ajout du type générique <T> pour l'implémentation des méthodes de Wrapper
+
 impl<T> Wrapper<T> {
+    // Modification de la signature de new pour accepter un argument value de type T
     pub fn new(value: T) -> Self {
         Wrapper { value }
     }
@@ -19,11 +24,13 @@ mod tests {
 
     #[test]
     fn store_u32_in_wrapper() {
+        // Pas besoin de spécifier le type générique car u32 peut être inféré
         assert_eq!(Wrapper::new(42).value, 42);
     }
 
     #[test]
     fn store_str_in_wrapper() {
+        // Spécification explicite du type générique &str car il ne peut pas être inféré
         assert_eq!(Wrapper::new("Foo").value, "Foo");
     }
 }
