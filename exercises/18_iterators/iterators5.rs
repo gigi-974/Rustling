a64f48257b854e6d0c57e05b6b24635b51a53f05
gigi-974 <127308830+gigi-974@users.noreply.github.com>
2024-04-26 10:53:45 +0200

    Update iterators5.rs

--- exercises/18_iterators/iterators5.rs
+++ exercises/18_iterators/iterators5.rs
@@ -33,6 +33,7 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // map is a hashmap with String keys and Progress values.
     // map = { "variables1": Complete, "from_str": None, ... }
+    // Implémentation de count_iterator en utilisant iter() et fold()
     map.iter().fold(0, |acc, (_, v)| { if v == &value {acc + 1} else {acc}})
 }
 
@@ -52,6 +53,7 @@ fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Pr
     // collection is a slice of hashmaps.
     // collection = [{ "variables1": Complete, "from_str": None, ... },
     //     { "variables2": Complete, ... }, ... ]
+    // Implémentation de count_collection_iterator en utilisant iter() et fold()
     collection.iter().fold(0, |acc, x| acc + count_iterator(x, value))
 }
 
