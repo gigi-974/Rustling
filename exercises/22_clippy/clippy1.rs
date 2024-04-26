9d3f3d7368080ae050f66887d67936203b492e0c
gigi-974 <127308830+gigi-974@users.noreply.github.com>
2024-04-26 12:24:42 +0200

    Update clippy1.rs

--- exercises/22_clippy/clippy1.rs
+++ exercises/22_clippy/clippy1.rs
@@ -6,13 +6,13 @@
 // check clippy's suggestions from the output to solve the exercise.
 // Execute `rustlings hint clippy1` or use the `hint` watch subcommand for a hint.
 
-use std::f32;
+// Utilisation directe de la constante PI de std::f32::consts au lieu d'une valeur littérale pour pi
+use std::f32::consts::PI;
 
 fn main() {
-    let pi = f32::consts::PI;
     let radius = 5.00f32;
-
-    let area = pi * f32::powi(radius, 2);
+    // Calcul de l'aire en utilisant directement la constante PI
+    let area = PI * f32::powi(radius, 2);
 
     println!(
         "The area of a circle with radius {:.2} is {:.5}!",
