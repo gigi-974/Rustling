9b0c9b91d17fa84e53a22ed49b6d193cb4dd950d
gigi-974 <127308830+gigi-974@users.noreply.github.com>
2024-04-26 10:24:35 +0200

    Update lifetimes3.rs

--- exercises/16_lifetimes/lifetimes3.rs
+++ exercises/16_lifetimes/lifetimes3.rs
@@ -2,8 +2,8 @@
 //
 // Lifetimes are also needed when structs hold references.
 //
-// Execute `rustlings hint lifetimes3` or use the `hint` watch subcommand for a hint.
-
+// Execute rustlings hint lifetimes3 or use the hint watch subcommand for a hint.
+// Ajout du paramètre de durée de vie 'a pour la struct Book
 struct Book<'a> {
     author: &'a str,
     title: &'a str,
@@ -12,7 +12,7 @@ struct Book<'a> {
 fn main() {
     let name = String::from("Jill Smith");
     let title = String::from("Fish Flying");
+    // Création d'une instance de Book en utilisant les références aux variables name et title
     let book = Book { author: &name, title: &title };
-
     println!("{} by {}", book.title, book.author);
 }
